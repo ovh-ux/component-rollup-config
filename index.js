52454f7817c3df7525e8219ff077df8349fedcfa
jleveugle <joffrey.leveugle@corp.ovh.com>
2018-10-22 11:07:28 +0200

    feat(name): name can now be override

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const commonjs = require('rollup-plugin-commonjs');
 const html = require('rollup-plugin-html');
 const babel = require('rollup-plugin-babel');
+const name = require('require-package-name');
 const postcss = require('rollup-plugin-postcss');
 const resolve = require('rollup-plugin-node-resolve');
 const path = require('path');
@@ -8,11 +9,13 @@ const fs = require('fs');
 const translations = require('./plugins/translations');
 const uiRouterTranslations = require('./plugins/ui-router-translations');
 
-module.exports = (opts) => {
+module.exports = (opts = {}) => {
   const workingDir = process.cwd();
   const packageConfig = JSON.parse(fs.readFileSync(path.resolve(workingDir, 'package.json')));
   const peerDeps = Object.keys(packageConfig.peerDependencies || {});
   let { translationsRoot } = opts;
+
+  const distFileName = opts.dist ? opts.dist.filename : `${name(packageConfig.name) || path.basename(workingDir)}.js`;
 
   if (!translationsRoot) {
     translationsRoot = fs.existsSync(path.resolve(process.cwd(), 'src'))
@@ -24,7 +27,7 @@ module.exports = (opts) => {
     input: opts.input,
     external: peerDeps.concat(opts.external || []),
     output: [{
-      file: `./dist/${path.basename(workingDir)}.js`,
+      file: `./dist/${distFileName}.js`,
       format: 'es',
       sourcemap: true,
     }],
